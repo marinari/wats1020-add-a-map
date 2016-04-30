a655dcd5ff02c5f39a1dc8c79caf4766d04df100
Arianna Marin <marin.arianna@gmail.com>
2016-04-30 12:18:00 -0700

    map functionality working proper default zoom added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 $( document ).ready(function() {
   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+  var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 16, attribution: osmAttrib});
 
   var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
@@ -22,18 +22,10 @@ $( document ).ready(function() {
       "Open Street Maps": osm
   }
 
-  var map = L.map('map-container').setView([47.621729, -122.350072], 13);
+  var map = L.map('map-container').setView([47.621729, -122.350072], 16);
 
   L.control.layers(mapLayers).addTo(map);
   satLayer.addTo(map);
-
-  var circle = L.circle([47.620391, -122.349074, 250, {
-    color: '#8B89BA',
-    fillColor: '#BEBBFF',
-    fillOpacity: 0.5
-  }).addTo(map);
-
-
 });
 
 
@@ -46,4 +38,10 @@ $( document ).ready(function() {
 /*
 var marker = L.marker([46.852, -121.760]).addTo(map);
 marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+
+var circle = L.circle([47.620391, -122.349074, 500, {
+  color: '#8B89BA',
+  fillColor: '#BEBBFF',
+  fillOpacity: 0.5
+}).addTo(map);
 */
