0c12c6cb1cc068ffa669addfb02d97a070e4360f
Arianna Marin <marin.arianna@gmail.com>
2016-04-30 12:03:02 -0700

    added circle to map

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 $( document ).ready(function() {
   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+  var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
 
   var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
@@ -27,8 +27,12 @@ $( document ).ready(function() {
   L.control.layers(mapLayers).addTo(map);
   satLayer.addTo(map);
 
-  var marker = L.marker([46.852, -121.760]).addTo(map);
-  marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+  var circle = L.circle([47.620391, -122.349074, 250, {
+    color: '#8B89BA',
+    fillColor: '#BEBBFF',
+    fillOpacity: 0.5
+  }).addTo(mymap);
+
 
 });
 
@@ -37,13 +41,7 @@ $( document ).ready(function() {
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
-/*
-var circle = L.circle([47.620426, -122.349149, 250, {
-  color: '#8B89BA',
-  fillColor: '#BEBBFF',
-  fillOpacity: 0.5
-}).addTo(mymap);
-*/
+
 
 /*
 var marker = L.marker([46.852, -121.760]).addTo(map);
