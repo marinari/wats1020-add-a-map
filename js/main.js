60a8c68255941a72cd97c3bfe8339472eeec66b8
Arianna Marin <marin.arianna@gmail.com>
2016-04-30 11:49:47 -0700

    mainjs with shawns code to test if it will work

--- js/main.js
+++ js/main.js
@@ -2,15 +2,10 @@
    rest of your functions inside the on ready handler.
    */
 $( document ).ready(function() {
-
-  // Inside of your on ready handler, invoke the Leaflet.js library to draw a map in your `#map-container` div.
-  var mymap = L.map('map-container').setView([47.622255, -122.351292], 13);
-
   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
   var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
-  //Add 2 layers to your map.
   var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
       subdomains: ['otile1','otile2','otile3','otile4']
@@ -26,8 +21,15 @@ $( document ).ready(function() {
       "Map View": drawLayer,
       "Open Street Maps": osm
   }
+
+  var map = L.map('map-container').setView([46.852, -121.760], 13);
+
   L.control.layers(mapLayers).addTo(map);
   satLayer.addTo(map);
+
+  var marker = L.marker([46.852, -121.760]).addTo(map);
+  marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+
 });
 
 
